0b4f280d8a0ad7e41b7995274e0b00d6bda049f4
Tim Ngeno <timngeno424@gmail.com>
2024-01-11 13:05:25 +0300

    return a type-annotated callback function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Write a type annotated function make_multiplier that takes a float
+`multiplier` and returns a function that multiplies a float by
+multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by the give multiplier
+
+    Args:
+        multiplier (float): the value by which the input will be
+        multiplied
+
+    Returns:
+        Callable[[float], float]: A function that takes a float as input
+    and returns the product from the multiplication
+    """
+    def multiply_function(x: float) -> float:
+        """
+        Returns the result of the multiplication between the float and
+        multiplier
+        """
+        return x * multiplier
+
+    return multiply_function
