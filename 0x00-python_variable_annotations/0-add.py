b7344fd1e79751017e9c08a3f321ae5be21bb301
Tim Ngeno <timngeno424@gmail.com>
2024-01-11 11:25:25 +0300

    implement a type-annotated add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Write a type annotated function `add` that takes a float `a` and
+float `b` as arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating point numbers and returns their sum
+
+    Args:
+        a (float): first argument
+        b (float): second argument
+    """
+    return (a + b)
