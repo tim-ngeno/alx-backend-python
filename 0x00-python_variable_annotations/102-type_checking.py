06574bf3708d4f6bf9902cea948af77ff488dce9
Tim Ngeno <timngeno424@gmail.com>
2024-01-11 16:19:11 +0300

    validate the piece of code using mypy

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Fix type annotation checks/errors"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return tuple(zoomed_in)
+
+
+array: Tuple[int, ...] = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
