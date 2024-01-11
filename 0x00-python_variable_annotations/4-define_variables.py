8413bc1a29b156aac4c8fe87b916f06298d2d6a4
Tim Ngeno <timngeno424@gmail.com>
2024-01-11 11:53:58 +0300

    define variables using type annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Define and annotate variables and assign the specified values
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
