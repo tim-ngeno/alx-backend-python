7af01f4bd67f974d11716b8a702a62af905158a5
Tim Ngeno <timngeno424@gmail.com>
2024-01-11 16:34:02 +0300

    fix type annotation checks

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,19 +5,17 @@ Fix type annotation checks/errors
 from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Return a tuple of integers
+    Returns a tuple of integers
     """
     zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-array: Tuple[int, ...] = (12, 72, 91)
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)
+array = [12, 72, 91]
+zoom_2x = zoom_array(tuple(array))
+zoom_3x = zoom_array(tuple(array), 3)
