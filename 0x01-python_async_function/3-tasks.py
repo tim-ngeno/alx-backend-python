e2c443e403c3efd3df9d8f1f841540fcd9d97a13
Tim Ngeno <timngeno424@gmail.com>
2024-01-15 15:08:30 +0300

    remove asyncio import

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """write a regular function to return a asyncio.Task object"""
 
-import asyncio
-
 
 def task_wait_random(max_delay: int):
     """
     Returns an asyncio.Task object
     """
     async def async_coroutine():
-        await asyncio.sleep(1)
+        pass
     return asyncio.create_task(async_coroutine())
