5cbd0b99ff3bc2cdb08e078e5ff878b2d26c962c
Tim Ngeno <timngeno424@gmail.com>
2024-01-16 21:23:43 +0300

    set return type annotation as list[float]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """
     Returns 10 random numbers from an async generator function
     """
