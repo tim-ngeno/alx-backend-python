941d9439d8f218cd43df5acb6b5ff2468cc68d3d
Tim Ngeno <timngeno424@gmail.com>
2024-01-16 19:33:02 +0300

    use an async generator to yield a random number btwn 0-10

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Write a coroutine that loops 10 times and yields a random number """
+
+import asyncio
+import random
+from typing import AsyncIterator
+
+
+async def async_generator() -> AsyncIterator[int]:
+    """
+    Runs a loop 10 times, waiting 1 second each time, and yields a
+    random number between 0 and 10
+    """
+    for _ in range(10):
+        yield random.randint(0, 10)
+        await asyncio.sleep(1)
