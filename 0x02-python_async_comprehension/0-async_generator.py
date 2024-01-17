78319e2b6618b05ba6a15d08cad323c213bd745a
Tim Ngeno <timngeno424@gmail.com>
2024-01-17 09:15:57 +0300

    use the Generator type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncIterator
+from typing import Generator
 
 
-async def async_generator() -> AsyncIterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Runs a loop 10 times, waiting 1 second each time, and yields a
     random number between 0 and 10
